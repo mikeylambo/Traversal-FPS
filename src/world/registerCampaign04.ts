e021373cf9ef502b3ba93080bc634206e184eb04
mikeylambo <mikeparker92@gmail.com>
2026-09-01 11:03:39 -0400

    Wire sectors 05-08 into campaign registration

--- src/world/registerCampaign04.ts
+++ src/world/registerCampaign04.ts
@@ -1,10 +1,14 @@
 import { CAMPAIGN_MAPS } from "./campaign";
 import { MAP_04_COURSE, MAP_04_FIELD } from "./campaign04";
+import { registerCampaign05to08 } from "./registerCampaign05to08";
 
 export function registerCampaign04(): void {
   const map = CAMPAIGN_MAPS.find((entry) => entry.id === "map-04");
-  if (!map) return;
-  map.implemented = true;
-  map.campaignRooms = MAP_04_FIELD;
-  map.courseRooms = MAP_04_COURSE;
+  if (map) {
+    map.implemented = true;
+    map.campaignRooms = MAP_04_FIELD;
+    map.courseRooms = MAP_04_COURSE;
+  }
+
+  registerCampaign05to08();
 }
